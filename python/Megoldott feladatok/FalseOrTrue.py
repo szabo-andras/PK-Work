e5abecf9f3016cd19735203f9b1915a921277e53
Szabó András <125799977+szabo-andras@users.noreply.github.com>
2023-10-03 13:25:32 +0200

    False or True?

---
+++ python/Megoldott feladatok/FalseOrTrue.py
@@ -0,0 +1,11 @@
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
+
+
+
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
