a7edc1e57c2dacc9a53beacd58c07364db1ca65b
Szabó András <125799977+szabo-andras@users.noreply.github.com>
2023-10-05 10:16:33 +0200

    Napok listájának szeletelése

---
+++ python/Megoldásra váró feladatok/napok_lista_szeletelése.py
@@ -0,0 +1,29 @@
+print("", end="\n\n")
+napok = ["Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"]
+print(napok)
+print("", end="\n\n")
+
+print(["Kedd", "Péntek"])
+print(napok[1:5:3])
+print("", end="\n\n")
+
+print(["Hétfő", "Vasárnap"])
+print(napok[0::6])
+print("", end="\n\n")
+
+print(["Szombat", "Szerda"])
+print(napok[5:1:-3])
+
+
+
+
+#ITT HAGYTAM ABBA
+print("", end="\n\n")
+
+print(["Vasárnap", "Szombat"])
+print(napok[-1:4:-1])
+
+print("", end="\n\n")
+
+print(["Hétfő", "Kedd", "Szerda"])
+print(napok[:3])
